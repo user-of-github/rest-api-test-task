7e31fcbeafb86ec0dd423994b8bc875bdad9304b
Nikita <nikitosminsk100@gmail.com>
2022-06-26 17:23:01 +0300

    Sending task for check

--- unit_test.py
+++ unit_test.py
@@ -6,7 +6,7 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
-API_BASEURL = 'http://127.0.0.1:8000'
+API_BASEURL = 'http://10.20.1.237'
 
 
 ROOT_ID = "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1"
